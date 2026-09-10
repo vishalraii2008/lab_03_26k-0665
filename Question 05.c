799224c8e98f502ebfb6b42cfac8f30ca52e3910
vishalraii2008 <vishallraii01@gmail.com>
2026-09-10 10:39:39 +0500

    Add files via upload

---
+++ Question 05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter first number a: ");
+    scanf("%d", &a);
+    printf("Enter second number b: ");
+    scanf("%d", &b);
+    printf("\nBefore swapping: a = %d, b = %d\n", a, b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping:  a = %d, b = %d\n", a, b);
+    return 0;
+}
